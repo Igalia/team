f13864f3df6ff91d74f87619172ded86aa9b7f28
Alexander Dunaev <adunaev@igalia.com>
2021-07-25 20:04:42 +0700

    Added another menu bar for selecting the current team. The team and the view are now stored in the session so one var persists when the other is changed.

--- skills/menus.py
+++ skills/menus.py
@@ -1,16 +1,29 @@
+from people.models import Team
+
+# noinspection PyUnresolvedReferences
 MENUS = {
-    'NAV_MENU_TOP': [
+    'NAV_MENU_1ST_LEVEL': [
+        {
+            'name': team.name,
+            'team_slug': team.slug,
+            'url': {'viewname': 'skills:set-current-team', 'kwargs': {'team_slug': team.slug}},
+        } for team in Team.objects.all()
+    ],
+    'NAV_MENU_2ND_LEVEL': [
         {
             "name": "Interest vs. Knowledge",
-            "url": "skills:interest-vs-knowledge"
+            "url": "skills:interest-vs-knowledge",
+            'root': True,
         },
         {
             "name": "Projects",
-            "url": "skills:projects"
+            "url": "skills:projects",
+            'root': True,
         },
         {
             "name": "Demand vs. Knowledge",
-            "url": "skills:demand-vs-knowledge"
+            "url": "skills:demand-vs-knowledge",
+            'root': True,
         },
-    ]
+    ],
 }
