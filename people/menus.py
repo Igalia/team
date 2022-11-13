2f2c8c0eefc1bbff655946931a0b0b42ab394e6d
Alexander Dunaev <adunaev@igalia.com>
2022-11-13 22:27:16 +0100

    Added the cross-application navigation. Added the stub avatar and some mor edata to display.

---
+++ people/menus.py
@@ -0,0 +1,14 @@
+MENUS = {
+    'NAV_MENU_1ST_LEVEL': [
+        {
+            "name": "People",
+            "url": "people:home",
+            'root': True,
+        },
+        {
+            "name": "Skills",
+            "url": "skills:home",
+            'root': True,
+        },
+    ],
+}
