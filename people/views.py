c96e4c716ca676a8f0781effcd187cf112565b0f
Alexander Dunaev <adunaev@igalia.com>
2022-11-13 17:07:08 +0100

    Improved the URL resolver.

--- people/views.py
+++ people/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http.response import Http404, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, resolve, Resolver404
 
 from .forms import SearchForm
 from .models import Person
@@ -14,9 +14,17 @@ def person(request, login):
             return HttpResponseRedirect(reverse('people:person', args=[search_form.cleaned_data['login']]))
 
         person = Person.objects.get(login=login)
+        people = Person.objects.all()
         return render(request,
                       'people/person.html',
                       {'person': person,
+                       'people': people,
                        'search_form': search_form})
     except Person.DoesNotExist:
+        try:
+            path = '/{login}/'.format(login=login)
+            resolve(path)   # If this fails, it will raise Resolver404.  Otherwise we just redirect to that URL.
+            return HttpResponseRedirect(path)
+        except Resolver404:
+            pass
         raise Http404("Person with id '{}' does not exist.".format(login))
