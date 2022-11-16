ff4c2f319b69e7ef405da404bbb6da8c9b671454
Alexander Dunaev <adunaev@igalia.com>
2022-11-16 22:50:33 +0100

    Updated the import script and the README.

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This is a web service that organises some aspects of the teamwork.
 
 The service is built on Django framework.  It consists of a set of *more or less* independent applications that handle different aspects of the work.  See READMEs in these applications to find more details.
+
+## Setting up
+
+The project depends on Python version 3 (3.10.7 was used in development) and a few Python packages listed in requirements.txt.  The recommended way of configuring this is using the Python virtual environment:
+
+    python3.10 -m venv "path/to/venv"
+    source "path/to/venv/bin/activate"
+    pip install -r requirements.txt
+
+Then create `team/site_settings.py` with the instance-sprecific configuration, using `team/site_settings.py.template` as template and following the advice given in that file.
+
+Finally, use the Django CLI tools to create the admin user, and set up your web and DB servers if necessary (not explained here).
--- people/management/commands/import_people.py
+++ people/management/commands/import_people.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 
 from django.core.management.base import BaseCommand
 
@@ -15,24 +16,33 @@ class Command(BaseCommand):
         with open(options['filename'], 'r') as inp:
             data = csv.reader(inp)
             for line in data:
-                login_str, level_str = line[0].strip(), line[1].strip()
+                # This is the CSV extracted from some internal people database
+                # (0) login, (1) Stage, 2, 3, (4) Full name, 5,
+                # 6, 7, (8) YYYY-MM-DD, 9, 10
+                login_str, level_str, full_name_str, join_date_str = line[0].strip(), line[1].strip(), line[4].strip(), line[8].strip()
+
                 try:
                     level = Level.objects.get(name=level_str)
 
-                    person = Person.objects.get(login=login_str)
-                    self.stdout.write('Skipping {person}: already registered.'.format(person=login_str))
-                    continue
+                    def get_person(login):
+                        try:
+                            person = Person.objects.get(login=login)
+                            self.stdout.write(self.style.SUCCESS('Updating {person}.'.format(person=login)))
+                        except Person.DoesNotExist:
+                            self.stdout.write(self.style.SUCCESS('Creating {person}.'.format(person=login)))
+                            person = Person(login=login)
+                        return person
+
+                    person = get_person(login_str)
+                    person.level = level
+                    person.full_name = full_name_str
+                    person.join_date = datetime.datetime.strptime(join_date_str, '%Y-%m-%d')
+                    person.save()
 
                 except Level.DoesNotExist:
                     self.stdout.write(
                         self.style.ERROR(
                             'Unknown level {level} specified for {person}!'.format(level=level_str, person=login_str)))
                     continue
-
-                except Person.DoesNotExist:
-                    person = Person(login=login_str, level=level)
-                    person.save()
-                    self.stdout.write(
-                        self.style.SUCCESS('Imported {person} as {level}'.format(person=login_str, level=level_str)))
 
         self.stdout.write(self.style.SUCCESS('Done.'))
