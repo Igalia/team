b620de81644c856aa6f55ab235bfc6df6d3571c0
Alex <alex-voodoo@users.noreply.github.com>
2022-11-17 23:36:16 +0100

    Implemented a fancy "User not found". (#43)
    This closes #38.

--- people/views.py
+++ people/views.py
@@ -1,5 +1,5 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http.response import Http404, HttpResponseRedirect
 from django.urls import reverse, resolve, Resolver404
 
 from .forms import SearchForm
@@ -7,9 +7,10 @@ from .models import Person, PersonalData
 
 
 def person(request, login):
-    try:
-        search_form = SearchForm(request.POST or None)
+    people = Person.objects.order_by('login')
+    search_form = SearchForm(request.POST or None)
 
+    try:
         if request.method == 'POST' and search_form.is_valid():
             return HttpResponseRedirect(reverse('people:person', args=[search_form.cleaned_data['login']]))
 
@@ -19,7 +20,6 @@ def person(request, login):
         except PersonalData.DoesNotExist:
             personal_data = PersonalData(person=person)
             personal_data.save()
-        people = Person.objects.order_by('login')
         return render(request,
                       'people/person.html',
                       {'person': person,
@@ -29,8 +29,11 @@ def person(request, login):
     except Person.DoesNotExist:
         try:
             path = '/{login}/'.format(login=login)
-            resolve(path)   # If this fails, it will raise Resolver404.  Otherwise we just redirect to that URL.
+            resolve(path)  # If this fails, it will raise Resolver404.  Otherwise we just redirect to that URL.
             return HttpResponseRedirect(path)
         except Resolver404:
             pass
-        raise Http404("Person with id '{}' does not exist.".format(login))
+        return render(request, 'people/person.html', {
+            'person': None,
+            'people': people,
+            'search_form': search_form})
