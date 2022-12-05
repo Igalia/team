c5b93fc86810d45bc0faea1650e9e7ea0b8d11b5
Alex <alex-voodoo@users.noreply.github.com>
2022-12-05 10:12:57 +0100

    Introduce inventory app (#50)
    Introduced the Inventory application.

---
+++ inventory/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.db.models.functions import Lower
+
+from inventory.models import DeviceType, Device, DeviceModel
+
+
+@admin.register(DeviceType)
+class DeviceTypeAdmin(admin.ModelAdmin):
+    ordering = (Lower('name'), )
+
+
+@admin.register(DeviceModel)
+class DeviceAdmin(admin.ModelAdmin):
+    ordering = ('brand', 'model_name')
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'assignee')
