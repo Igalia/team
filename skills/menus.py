04659d8594da9465b5520efb0d1f78e4473b9556
Alex <alex-voodoo@users.noreply.github.com>
2023-12-11 23:07:54 +0700

    Re-organised the menus and arranged logically the People part (#101)
    * Re-organised the menus and arranged logically the People part
    
    * Fixed formatting and improved the comment

--- skills/menus.py
+++ skills/menus.py
@@ -1,5 +1,14 @@
 from people.models import Team
 
+
+def in_app(request):
+    """Returns whether `request` would be handled by this app.
+
+    This is a validator for the menu.  It hides the Skills second level menu when views from other apps are rendered.
+    """
+    return 'skills' in request.resolver_match.app_names
+
+
 # noinspection PyUnresolvedReferences
 MENUS = {
     "NAV_MENU_1ST_LEVEL": [],
@@ -7,19 +16,20 @@ MENUS = {
         {
             "name": "Interest vs. Knowledge",
             "url": "skills:interest-vs-knowledge",
-            "root": True,
+            "root": False,
+            "validators": ["skills.menus.in_app"],
         },
         {
             "name": "Projects",
             "url": "skills:projects",
-            "root": True,
-            "validators": ["skills.views.enable_projects"],
+            "root": False,
+            "validators": ["skills.views.enable_projects", "skills.menus.in_app"],
         },
         {
             "name": "Demand vs. Knowledge",
             "url": "skills:demand-vs-knowledge",
             "root": True,
-            "validators": ["skills.views.enable_projects"],
+            "validators": ["skills.views.enable_projects", "skills.menus.in_app"],
         },
     ],
     "NAV_MENU_TEAM_SELECTOR": [
